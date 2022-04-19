163ec9b0e4d815819682dcde067bbc0118e8fc20
Kanishka Rajput <97453078+KanishkaRajputd@users.noreply.github.com>
2022-04-19 19:19:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useRef, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [timer, setTimer] = useState(0)
+const timerRef=useRef(null)
+
+  useEffect(()=>{
+  timerRef.current=setInterval(()=>{
+    console.log("Heloo")
+setTimer((p)=>{
+
+  if(p==100){
+    clearInterval(timerRef.current);
+    return 100
+  }
+
+   return p=p+1;
+
+})
+
+
+return ()=>{
+  clearInterval(timerRef.current);
+}
+  },1000)
+
+},[])
+
+
+
+  return (
+    <div className="App">
+      <h1>TIMER</h1>
+      <h1>{timer}</h1>
+      <button
+      onClick={()=>{
+        clearInterval(timerRef.current)
+      }}
+      
+      >Pause</button>
+      <button
+      onClick={()=>{
+        timerRef.current=setInterval(()=>{
+          console.log("Heloo")
+      setTimer((p)=>{
+      
+        if(p==100){
+          clearInterval(timerRef.current);
+          return 100
+        }
+      
+         return p=p+1;
+      
+      })
+    },1000)
+      
+      }}
+      
+      >Start</button>
+      <button
+      onClick={()=>{
+        clearInterval(timerRef.current)
+        setTimer(0);
+      }}
+      >Reset</button>  
+
+    </div>
+  )
+}
+
+export default App
